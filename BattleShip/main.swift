aae2b30ef189eae2bff15b5bbd7fcb7bc7f43dc3
Reid, Jon Carl <jreid2@hk220-mac2.local>
2018-09-10 11:20:54 -0700

    Initial Commit

---
+++ BattleShip/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BattleShip
+//
+//  Created by Reid, Jon Carl on 9/10/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
